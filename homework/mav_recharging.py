3f8fa34e57197ddee4af589ae661bed707d5f032
Bryan A. Jones <bjones AT ece.msstate.edu>
2015-09-18 10:53:12 -0500

    Add: Paramtereize tests to fly multiple missions. Docs: Add headings.

--- homework/mav_recharging.py
+++ homework/mav_recharging.py
@@ -34,9 +34,13 @@ class Enum(frozenset):
         raise AttributeError
 
 _MAV_STATES = Enum( ('Flying', 'Waiting', 'Charging') )
-
+#
+# Code
+# ====
+# The core code in this module.
+#
 # MAV class
-# =========
+# ---------
 class MAV(Thread):
     def __init__(self,
       # The left electrode for this MAV.
@@ -71,6 +75,8 @@ class MAV(Thread):
 #
 # Testing
 # =======
+# MockElectrode class
+# -------------------
 # A testable electrode: waits until True is placed in its queue before allowing code to proceed.
 class MockElectrode(object):
     def __init__(self):
@@ -78,7 +84,7 @@ class MockElectrode(object):
         self.is_locked = False
 
     # Context manager
-    # ---------------
+    # ^^^^^^^^^^^^^^^
     # Defining these methods allows use of the ``MockElectrode`` class in a `context manager <https://docs.python.org/2/reference/datamodel.html#context-managers>`_ (the `with <https://docs.python.org/2/reference/compound_stmts.html#with>`_ statement).
     #
     # To acquire a (mock) electrode, wait until the test grants permission to use this electrode by plaing a True value in its queue. See `__enter__ <https://docs.python.org/2/reference/datamodel.html#object.__enter__>`_.
@@ -91,17 +97,75 @@ class MockElectrode(object):
     def __exit__(self, exc_type, exc_value, traceback):
         self.is_locked = False
         return False
+#
+# Helper functions
+# ----------------
+# These support testing.
+#
+# last_is_different
+# ^^^^^^^^^^^^^^^^^
+# A helper function: returns a list of n items, in which all but the last item
+# is beginning_item. The last item is end_item.
+def last_is_different(
+ # The object which is placed as the first n - 1 items in the returned list.
+ beginning_item,
+ # The object placed at the end of the list.
+ end_item,
+ # .. _n:
+ #
+ # The number of items in the list. Must be an integer greather than 0.
+ n):
 
+    assert n > 0
+    return [beginning_item]*(n - 1) + [end_item]
+#
+# last_is_false
+# ^^^^^^^^^^^^^
+# A helper function: return a list in which the last item is False and all other
+# items are True.
+def last_is_false(
+  # See n_.
+  n):
+
+    return last_is_different(True, False, n)
+#
+# TestlastIsDifferent class
+# -------------------------
+class TestLastIsDifferent(object):
+    # Test with a one-element list.
+    def test_1(self):
+        assert last_is_different('a', 'b', 1) == ['b']
+
+    # Test with a two-element list.
+    def test_2(self):
+        assert last_is_different('a', 'b', 2) == ['a', 'b']
+
+    # Test with a > 2 element list.
+    def test_3(self):
+        assert last_is_different(1, 2, 3) == [1, 1, 2]
+
+    def test_4(self):
+        with pytest.raises(AssertionError):
+            last_is_different(1.1, 2.2, 0)
+#
+# TestMav class
+# -------------
 class TestMav(object):
-    def one_mission(self,
+    # Fly a set of missions, testing at all times.
+    def fly_missions(self,
           # See fly_time_sec_.
           fly_time_sec,
           # See charge_time_sec_.
-          charge_time_sec):
+          charge_time_sec,
+          # Number of fly/land/charge missions to fly.
+          missions):
 
+        # The time to wait for the MAV to take an action.
+        thread_switch_time_sec = 0.01
+
+        # Set up the MAV.
         e_left = MockElectrode()
         e_right = MockElectrode()
-        thread_switch_time_sec = 0.01
         m = MAV(e_left, e_right, fly_time_sec, charge_time_sec)
         assert m._state == None
 
@@ -113,48 +177,54 @@ class TestMav(object):
         # Use a ``finally`` clause to guarentee that the ``m`` thread will be
         # shut down properly.
         try:
-            # Wait for the thread to start, then check that it's flying.
-            sleep(thread_switch_time_sec)
-            assert m._state == _MAV_STATES.Flying
+            for running in last_is_false(missions):
 
-            # Wait for the fly time to end, then check that it's waiting.
-            sleep(fly_time_sec)
-            assert m._state == _MAV_STATES.Waiting
+                # Wait for the thread to start, then check that it's flying.
+                sleep(thread_switch_time_sec)
+                assert m._state == _MAV_STATES.Flying
 
-            # Wait a while to make sure it's waiting for an electrode.
-            sleep(charge_time_sec*2)
-            assert m._state == _MAV_STATES.Waiting
-            e_left.q.put(True)
+                # Wait for the fly time to end, then check that it's waiting.
+                sleep(fly_time_sec)
+                assert m._state == _MAV_STATES.Waiting
 
-            # Wait some more to make sure it's waiting for the other electrode.
-            sleep(charge_time_sec*2)
-            assert m._state == _MAV_STATES.Waiting
-            e_right.q.put(True)
+                # Wait a while to make sure it's waiting for an electrode.
+                sleep(charge_time_sec*2)
+                assert m._state == _MAV_STATES.Waiting
+                e_left.q.put(True)
 
-            # Wait a bit to let it start charging.
-            sleep(thread_switch_time_sec)
-            m.running = False
-            assert m._state == _MAV_STATES.Charging
+                # Wait some more to make sure it's waiting for the other
+                # electrode.
+                sleep(charge_time_sec*2)
+                assert m._state == _MAV_STATES.Waiting
+                e_right.q.put(True)
 
-            # Wait for the charge cycle to finish. The MAV should be done.
-            sleep(charge_time_sec)
-            assert m._state == None
+                # Wait a bit to let it start charging.
+                sleep(thread_switch_time_sec)
+                m.running = running
+                assert m._state == _MAV_STATES.Charging
+
+                # Wait for the charge cycle to finish. The MAV should be done.
+                sleep(charge_time_sec)
 
         finally:
             # Tell the MAV to stop operations.
             m.running = False
-            # Wait for the thread to terminate.
+            # Wait a bit for the thread to terminate.
             m.join(thread_switch_time_sec)
-            # Verify that it exited correctly -- the thread should be dead.
+            # Verify that it exited correctly -- the thread should be dead and
+            # the state None.
             assert not m.isAlive()
+            assert m._state == None
 
     # Single mission tests.
     def test_1(self):
-        self.one_mission(0.05, 0.15)
+        self.fly_missions(0.05, 0.15, 1)
 
     def test_2(self):
-        self.one_mission(0.10, 0.20)
-
+        self.fly_missions(0.10, 0.20, 2)
+#
+# TestElectrode class
+# -------------------
 class TestElectrode(object):
     # Releasing an electrode not in use should raise an exception.
     def test_1(self):
